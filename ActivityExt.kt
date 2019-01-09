98fdbba95c0b99902c44eff986ecad1af638f151
irfanirawansukirman <dadang.kotz@gmail.com>
2019-01-09 17:43:19 +0700

    update content/move code to right place :D

--- ActivityExt.kt
+++ ActivityExt.kt
@@ -1,26 +1,13 @@
 /* Extentions activity level */
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.PopupMenu
-import android.text.TextUtils
 import android.view.View
-import android.widget.Toast
-import timber.log.Timber
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-import java.util.*
 
 /**
  * Using it for replacement fragment
@@ -67,28 +54,6 @@ fun AppCompatActivity.replaceFragmentInActivityWithBackStack(
     //=======================================
 }
 
-/**
- * Using it for showing toast in activity level
- *
- * @param activityContext => state app
- * @param message => text info
- */
-fun AppCompatActivity.showToast(
-    activityContext: Context,
-    message: String
-) {
-    Toast.makeText(
-        activityContext, if (TextUtils.isEmpty(message))
-            Const.SERVER_ERROR_MESSAGE_DEFAULT else message, Toast.LENGTH_SHORT
-    ).show()
-
-    //=========== How to using it ===========
-    //override fun onYourMethod(errorMessage: String) {
-    //    showToast(this, errorMessage)
-    //}
-    //=======================================
-}
-
 private inline fun FragmentManager.transact(
     action: FragmentTransaction.() -> Unit
 ) {
@@ -123,198 +88,6 @@ fun AppCompatActivity.transparentStatusBar(
 }
 
 /**
- * Using it for move to another page without param
- */
-inline fun <reified T : AppCompatActivity> AppCompatActivity.navigator(
-
-) {
-    val intent = Intent(this, T::class.java)
-    startActivity(intent)
-
-    //=========== How to using it ===========
-    // navigator<YourActivity>()
-    //=======================================
-}
-
-/**
- * Using it for move to another page with a single param
- * If you want sand data using object, model, etc convert it into string using Gson
- *
- * @param param => param with string type
- */
-inline fun <reified T : AppCompatActivity> AppCompatActivity.navigator(
-    param: String
-) {
-    val intent = Intent(this, T::class.java)
-    intent.putExtra("param", param)
-    startActivity(intent)
-
-    //=========== How to using it ===========
-    // navigator<YourActivity>(yourParamObj)
-    //=======================================
-}
-
-/**
- * Using it for moving to another page with activity package name (usually modular package)
- *
- * @param activityPackage => exp : id.co.gits.feature_home_detail.HomeDetailActivity
- * @param activityContext => your state app
- */
-fun AppCompatActivity.navigatorImplicit(
-    activityContext: Context,
-    activityPackage: String
-) {
-    val intent = Intent()
-    try {
-        intent.setClass(
-            activityContext,
-            Class.forName(activityPackage)
-        )
-        startActivity(intent)
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
-
-    //=========== How to using it ===========
-    // navigatorImplicit(context, yourActivityPackageName)
-    //=======================================
-}
-
-/**
- * Using it for loggin debug data using Timber
- *
- * @param TAG => your TAG usually the name of activity class
- * @param message => text info
- */
-fun AppCompatActivity.logD(
-    TAG: AppCompatActivity,
-    message: String
-) {
-    Timber.d("${TAG::class.java.simpleName} $message")
-
-    //=========== How to using it ===========
-    // logD(YourActivity(), yourMessage)
-    //=======================================
-}
-
-/**
- * Using it for logging verbose data using Timber
- *
- * @param TAG => your TAG usually the name of activity class
- * @param message => text info
- */
-fun AppCompatActivity.logV(
-    TAG: AppCompatActivity,
-    message: String
-) {
-    Timber.v("${TAG::class.java.simpleName} $message")
-
-    //=========== How to using it ===========
-    // logV(YourActivity(), yourMessage)
-    //=======================================
-}
-
-/**
- * Using it for logging error data using Timber
- *
- * @param TAG => your TAG usually the name of activity class
- * @param message => text info
- */
-fun AppCompatActivity.logE(
-    TAG: AppCompatActivity,
-    message: String
-) {
-    Timber.e("${TAG::class.java.simpleName} $message")
-
-    //=========== How to using it ===========
-    // logE(YourActivity(), yourMessage)
-    //=======================================
-}
-
-/**
- * Using it for saving bitmap from imageview to image file
- *
- * @param context => state app
- * @param imageBitmap => bitmap from your imageview
- * @param directoryName => for naming your directory for saving image view
- * @param showMessageStatus => message status if you failed or success creating image file
- */
-@SuppressLint("ObsoleteSdkInt")
-fun AppCompatActivity.saveBitmapToLocalFile(
-    context: Context,
-    imageBitmap: Bitmap,
-    directoryName: String?,
-    showMessageStatus: Boolean
-) {
-    val root = Environment.getExternalStorageDirectory().toString()
-
-    val directoryNameDefault = if (TextUtils.isEmpty(directoryName)) {
-        Const.APP_FOLDER_DEFAULT
-    } else {
-        directoryName
-    }
-
-    val myDir = File("$root/$directoryNameDefault")
-
-    if (!myDir.exists()) {
-        myDir.mkdirs()
-    }
-
-    val random = Random()
-    val numbers = 100
-    val numberResult = random.nextInt(numbers)
-    val imageFileName = "IMG_$numberResult.png"
-    val existImageFile = File(myDir, imageFileName)
-    val outStream: FileOutputStream
-    val bitmap = imageBitmap
-    var isSuccessFileSaving: Boolean
-
-    try {
-        outStream = FileOutputStream(existImageFile)
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
-        /* 100 to keep full quality of the image */
-        outStream.flush()
-        outStream.close()
-        isSuccessFileSaving = true
-    } catch (e: FileNotFoundException) {
-        e.printStackTrace()
-        isSuccessFileSaving = false
-    } catch (e: IOException) {
-        e.printStackTrace()
-        isSuccessFileSaving = false
-    }
-
-    val message = if (isSuccessFileSaving) {
-        Const.MESSAGE_SUCCESS_IMAGE_SAVE
-    } else {
-        Const.MESSAGE_FAILED_IMAGE_SAVE
-    }
-
-    if (showMessageStatus) showToast(context, message)
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-        val scanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
-        val contentUri = Uri.fromFile(existImageFile)
-        scanIntent.data = contentUri
-        context.sendBroadcast(scanIntent)
-    } else {
-        context.sendBroadcast(
-            Intent(
-                Intent.ACTION_MEDIA_MOUNTED,
-                Uri.parse(
-                    Const.SDCARD_URI_PATH + Environment
-                        .getExternalStorageDirectory()
-                )
-            )
-        )
-    }
-
-    //=========== How to using it ===========
-    // saveBitmapToLocalFile(activityContenxt, yourBitmap, dirName, yourBooleanStatus)
-    //=======================================
-}
-
-/**
  * Using it for show icon inside popup menu
  *
  * @param popupMenu => your popup menu
@@ -337,7 +110,7 @@ fun AppCompatActivity.setForceShowIcon(popupMenu: PopupMenu) {
     } catch (e: Throwable) {
         e.printStackTrace()
     }
-    
+
     //=========== How to using it ===========
     // popUp.apply {
     //    dismiss()
--- ContextExt.kt
+++ ContextExt.kt
@@ -4,6 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import timber.log.Timber
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
 
 /**
  * Using it for move to another page with clearing existing activity stack
@@ -20,43 +30,53 @@ inline fun <reified T : AppCompatActivity> Context.navigatorWithActivityClearTop
     //=======================================
 }
 
+/**
+ * Using it for move to another page without param
+ */
 inline fun <reified T : AppCompatActivity> Context.navigator(
 
 ) {
     val intent = Intent(this, T::class.java)
     startActivity(intent)
+
+    //=========== How to using it ===========
+    // navigator<YourActivity>()
+    //=======================================
 }
 
-inline fun <reified T : AppCompatActivity> Context.navigator(
-    param: String
-) {
-    val intent = Intent(this, T::class.java)
-    intent.putExtra("param", param)
-    startActivity(intent)
-}
-
+/**
+ * Using it for move to another page with a single param
+ * If you want sand data using object, model, etc convert it into string using Gson
+ *
+ * @param param => param with string type
+ */
 inline fun <reified T : AppCompatActivity> Context.navigator(
     intentParams : Intent.() -> Unit
 ) {
     val intent = Intent(this, T::class.java)
     intent.intentParams()
     startActivity(intent)
+
+    //=========== How to using it ===========
+    // navigator<DetailActivity> {
+    //        putExtra("KEY1" , "VALUE1")
+    //        putExtra("KEY2" , "VALUE2")
+    //    }
+    //=======================================
 }
 
 /**
- * Using it for moving to another page with activity package name (usually modular package)
+ * Using it for moving to another page with activity package name (usually modular package) without params
  *
  * @param activityPackage => exp : id.co.gits.feature_home_detail.HomeDetailActivity
- * @param activityContext => your state app
  */
 fun Context.navigatorImplicit(
-    activityContext: Context,
     activityPackage: String
 ) {
     val intent = Intent()
     try {
         intent.setClass(
-            activityContext,
+            this,
             Class.forName(activityPackage)
         )
         startActivity(intent)
@@ -65,7 +85,36 @@ fun Context.navigatorImplicit(
     }
 
     //=========== How to using it ===========
-    // navigatorImplicit(context, yourActivityPackageName)
+    // navigatorImplicit(yourActivityPackageName)
+    //=======================================
+}
+
+/**
+ * Using it for moving to another page with activity package name (usually modular package) with params
+ *
+ * @param activityPackage => exp : id.co.gits.feature_home_detail.HomeDetailActivity
+ */
+fun Context.navigatorImplicit(
+    activityPackage: String,
+    intentParams : Intent.() -> Unit
+) {
+    val intent = Intent()
+    try {
+        intent.intentParams()
+        intent.setClass(
+            this,
+            Class.forName(activityPackage)
+        )
+        startActivity(intent)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    //=========== How to using it ===========
+    // navigatorImplicit(yourActivityPackageName) {
+    //        putExtra("KEY1" , "VALUE1")
+    //        putExtra("KEY2" , "VALUE2")
+    //    }
     //=======================================
 }
 
@@ -73,4 +122,138 @@ fun Context.showToast(
     message: String
 ) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * Using it for loggin debug data using Timber
+ *
+ * @param TAG => your TAG usually the name of activity class
+ * @param message => text info
+ */
+fun Context.logD(
+    TAG: AppCompatActivity,
+    message: String
+) {
+    Timber.d("${TAG::class.java.simpleName} $message")
+
+    //=========== How to using it ===========
+    // logD(YourActivity(), yourMessage)
+    //=======================================
+}
+
+/**
+ * Using it for logging verbose data using Timber
+ *
+ * @param TAG => your TAG usually the name of activity class
+ * @param message => text info
+ */
+fun Context.logV(
+    TAG: AppCompatActivity,
+    message: String
+) {
+    Timber.v("${TAG::class.java.simpleName} $message")
+
+    //=========== How to using it ===========
+    // logV(YourActivity(), yourMessage)
+    //=======================================
+}
+
+/**
+ * Using it for logging error data using Timber
+ *
+ * @param TAG => your TAG usually the name of activity class
+ * @param message => text info
+ */
+fun Context.logE(
+    TAG: AppCompatActivity,
+    message: String
+) {
+    Timber.e("${TAG::class.java.simpleName} $message")
+
+    //=========== How to using it ===========
+    // logE(YourActivity(), yourMessage)
+    //=======================================
+}
+
+/**
+ * Using it for saving bitmap from imageview to image file
+ *
+ * @param context => state app
+ * @param imageBitmap => bitmap from your imageview
+ * @param directoryName => for naming your directory for saving image view
+ * @param showMessageStatus => message status if you failed or success creating image file
+ */
+@SuppressLint("ObsoleteSdkInt")
+fun Context.saveBitmapToLocalFile(
+    context: Context,
+    imageBitmap: Bitmap,
+    directoryName: String?,
+    showMessageStatus: Boolean
+) {
+    val root = Environment.getExternalStorageDirectory().toString()
+
+    val directoryNameDefault = if (TextUtils.isEmpty(directoryName)) {
+        Const.APP_FOLDER_DEFAULT
+    } else {
+        directoryName
+    }
+
+    val myDir = File("$root/$directoryNameDefault")
+
+    if (!myDir.exists()) {
+        myDir.mkdirs()
+    }
+
+    val random = Random()
+    val numbers = 100
+    val numberResult = random.nextInt(numbers)
+    val imageFileName = "IMG_$numberResult.png"
+    val existImageFile = File(myDir, imageFileName)
+    val outStream: FileOutputStream
+    val bitmap = imageBitmap
+    var isSuccessFileSaving: Boolean
+
+    try {
+        outStream = FileOutputStream(existImageFile)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
+        /* 100 to keep full quality of the image */
+        outStream.flush()
+        outStream.close()
+        isSuccessFileSaving = true
+    } catch (e: FileNotFoundException) {
+        e.printStackTrace()
+        isSuccessFileSaving = false
+    } catch (e: IOException) {
+        e.printStackTrace()
+        isSuccessFileSaving = false
+    }
+
+    val message = if (isSuccessFileSaving) {
+        Const.MESSAGE_SUCCESS_IMAGE_SAVE
+    } else {
+        Const.MESSAGE_FAILED_IMAGE_SAVE
+    }
+
+    if (showMessageStatus) showToast(context, message)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        val scanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val contentUri = Uri.fromFile(existImageFile)
+        scanIntent.data = contentUri
+        context.sendBroadcast(scanIntent)
+    } else {
+        context.sendBroadcast(
+            Intent(
+                Intent.ACTION_MEDIA_MOUNTED,
+                Uri.parse(
+                    Const.SDCARD_URI_PATH + Environment
+                        .getExternalStorageDirectory()
+                )
+            )
+        )
+    }
+
+    //=========== How to using it ===========
+    // saveBitmapToLocalFile(activityContenxt, yourBitmap, dirName, yourBooleanStatus)
+    //=======================================
 }
